7ce09b108d1822e6736898305eb589b856448b5c
chrisjaechun <chrisjaechun@gmail.com>
2021-01-07 14:33:21 -0500

    Added deployed Heroku site for production
    - Updated apiUrl production path in config.js to Heroku

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,52 @@
+const config = require('./../config.js')
+const store = require('./../store.js')
+
+// API call for sign-up
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
+
+// API call for sign-in
+const signIn = function (data) {
+    return $.ajax({
+      url: config.apiUrl + '/sign-in',
+      method: 'POST',
+      data: data
+    })
+  }
+  
+// API call for change-password
+const changePassword = function (data) {
+    return $.ajax({
+      url: config.apiUrl + '/change-password',
+      method: 'PATCH',
+      data: data,
+      headers: {
+        Authorization: 'Bearer ' + store.user.token
+      }
+    })
+  }
+  
+  // API call for sign-out
+  const signOut = function (data) {
+    return $.ajax({
+      url: config.apiUrl + '/sign-out',
+      method: 'DELETE',
+      data: data,
+      headers: {
+        Authorization: 'Bearer ' + store.user.token
+      }
+    })
+  }
+  
+  module.exports = {
+    signUp,
+    signIn,
+    changePassword,
+    signOut
+  }
+  
